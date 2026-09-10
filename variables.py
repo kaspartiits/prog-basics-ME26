1984581a4b52cc50a0a709ea065bb863bfc1d4a9
kaspartiits <kaspar.tiits.002@voco.ee>
2026-09-10 11:44:14 +0000

    Muutujate ülesanne tehtud

---
+++ variables.py
@@ -0,0 +1,19 @@
+"""
+*1.* Luua muutuja `word` ja anda väärtuseks "spider".
+
+*2.* Luua muutuja `thousand` ja anda väärtuseks "1000".
+
+*3.* Luua muutuja `ten` ja anda väärtuseks 10.
+
+*4.* Luua muutuja `first_half` väärtusega "full".
+
+*5.* Luua muutuja `second_half` väärtusega "moon".
+
+*6.* Luua muutuja `complete` väärtusega "full moon", kasutades selleks eelnevalt loodud muutujaid.
+"""
+word = "spider" \
+thousand = "1000" \
+ten = 10
+first_half = "full"
+second_half = "moon"
+complete = first_half + " " + second_half
